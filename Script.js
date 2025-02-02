b98cdbd5886cb6d0081cda434f642227f5f62e77
rohitrot738 <araj5945@gmail.com>
2025-02-02 17:11:30 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-function loadPage() {
-  var url = document.getElementById("url").value;
-  if (url) {
-    document.getElementById("browser").src = url;
-  } else {
-    alert("Please enter a valid URL.");
-  }
-}
